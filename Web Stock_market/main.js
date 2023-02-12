3bbf76f06fc7020478036e7d21a9e0d594583ef3
@nhTuan <tuan30vh@gmail.com>
2023-02-13 04:43:51 +0700

    update

---
+++ Web Stock_market/main.js
@@ -0,0 +1,51 @@
+async function checkMyStock() {
+    let x = document.getElementById("stock").value;
+    console.log(x)
+    if (x == "Chose one stock ...."){
+      alert("Hãy chọn mã cổ phiếu muốn dự đoán")
+    }else{
+        const stock = x
+        console.log(x)
+        const response  = await fetch("./data.json");
+        const {rows} = await response.json();
+        console.log(rows)
+        for(const row of rows){
+            if (stock == row[0]){
+                document.getElementById("result").innerHTML = "Giá cổ phiếu "+stock+ " được dự đoán là " + row[1]+ "(VND)";
+            }
+        }
+    }
+}
+async function loadIntoTable(url, table){
+    const tableBody = table.querySelector("tbody");
+    const response = await fetch(url);
+    const { rows} = await response.json();
+    console.log(("STB" == rows[0][0]))
+    // tableBody.innerHTML = "<tr></tr>";
+    for (const row of rows){
+        check_color = row[2]
+        console.log(check_color)
+        const rowElement = document.createElement("tr");
+        // if(check_color == 1){
+        //     rowElement.setAttribute('id','increase');
+        // }else{
+        //     rowElement.setAttribute('id','reduce');
+        // }
+        row_new = [row[0], row[1]]
+        for (const cellText of row_new){
+            const cellElement = document.createElement("th");
+            cellElement.textContent = cellText;
+            rowElement.appendChild(cellElement);
+            
+        }
+        tableBody.appendChild(rowElement);
+
+
+    }
+    document.getElementById("increase").style.color = "green";
+    document.getElementById("reduce").style.color = "red";
+}
+
+
+loadIntoTable("./data.json", document.querySelector("table"));
+
